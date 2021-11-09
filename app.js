2cee7b6fc79e60b2d714e428e0d4bea0353cf3bf
Akash VG <akashvg007@gmail.com>
2021-11-09 23:59:26 +0530

    online offline check

--- app.js
+++ app.js
@@ -11,11 +11,26 @@ const io = socketio(server);
 app.use(cors())
 
 const port = process.env.PORT || 5000
-
+const USERS = [];
 io.on('connection', socket => {
     const id = socket.handshake.query.id
     socket.join(id)
-
+    console.log("id", id);
+    if (!USERS.includes(id)) USERS.push(id);
+    // socket.broadcast.emit('online', { id, status: 1 })
+    setInterval(() => {
+        USERS.forEach(user => {
+            const others = USERS.filter(u => u !== user);
+            socket.broadcast.to(user).emit('online', { ids: others, status: 1 })
+        })
+    }, 10000)
+    socket.on('disconnect', (data) => {
+        const user = socket.handshake.query.id;
+        console.log("disconnected", data, socket.handshake.query.id);
+        const index = USERS.indexOf(user)
+        if (index > -1) USERS.splice(index, 1);
+        socket.broadcast.emit('offline', { id: user, status: 2 })
+    })
     socket.on('send-message', ({ recipient, text }) => {
         console.log("message::text", recipient, text);
 
