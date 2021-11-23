4b4a241d4dd4ebbae92bae86212c6c06b5c27c79
Akash VG <akashvg007@gmail.com>
2021-11-23 19:15:18 +0530

    socket changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
-
+require('dotenv').config()
 
 const app = express();
 const server = http.createServer(app);
@@ -17,26 +17,27 @@ io.on('connection', socket => {
     socket.join(id)
     console.log("id", id);
     if (!USERS.includes(id)) USERS.push(id);
-    // socket.broadcast.emit('online', { id, status: 1 })
     setInterval(() => {
         USERS.forEach(user => {
             const others = USERS.filter(u => u !== user);
             socket.broadcast.to(user).emit('online', { ids: others, status: 1 })
         })
     }, 10000)
+
+    socket.on('send-message', ({ recipient, text }) => {
+        console.log("message::text", recipient, text);
+        console.log("message::send", recipient, id, text);
+        socket.broadcast.to(recipient).emit('receive', { recipient, sender: id, text })
+        socket.broadcast.to(recipient).emit('receive-message', {
+            recipient, sender: id, text
+        })
+    })
     socket.on('disconnect', (data) => {
         const user = socket.handshake.query.id;
         console.log("disconnected", data, socket.handshake.query.id);
         const index = USERS.indexOf(user)
         if (index > -1) USERS.splice(index, 1);
         socket.broadcast.emit('offline', { id: user, status: 2 })
-    })
-    socket.on('send-message', ({ recipient, text }) => {
-        console.log("message::text", recipient, text);
-
-        socket.broadcast.to(recipient).emit('receive-message', {
-            recipient, sender: id, text
-        })
     })
 })
 
